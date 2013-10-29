e116412f2e29c4e7ef922e6e728c4c610bcc1660
mih <minimih@gmail.com>
2013-10-29 20:31:25 +0100

    08

---
+++ web/Selbststudium/Veranstaltung08/php-classes/Lebewesen.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class Lebewesen {
+    protected $alter = 0;
+    
+    abstract public function altern();
+
+    /*-----------------------------------------*/
+    //getter/setter
+    
+    public function getAlter(){
+        echo "$this->alter <br/>";        
+    }
+}
+
+?>
