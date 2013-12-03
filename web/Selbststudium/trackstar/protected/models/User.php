096c8c5fde986a1281210d02b60198c6aa93c9c7
mih <minimih@gmail.com>
2013-12-03 21:17:52 +0100

    kapitel 6

--- web/Selbststudium/trackstar/protected/models/User.php
+++ web/Selbststudium/trackstar/protected/models/User.php
@@ -19,9 +19,11 @@
  * @property Issue[] $issues1
  * @property Project[] $tblProjects
  */
-class User extends CActiveRecord
+class User extends TrackStarActiveRecord
 {
-	/**
+	public $password_repeat;
+
+    /**
 	 * @return string the associated database table name
 	 */
 	public function tableName()
@@ -37,10 +39,12 @@ class User extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('username, email, password', 'required'),
-			array('create_user_id, update_user_id', 'numerical', 'integerOnly'=>true),
+			array('username, email, password, password_repeat', 'required'),
 			array('username, email, password', 'length', 'max'=>255),
-			array('last_login_time, create_time, update_time', 'safe'),
+            array('email, username', 'unique'),
+            array('email', 'email'),
+            array('password', 'compare'),
+            array('password_repeat', 'safe'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
 			array('id, username, email, password, last_login_time, create_time, create_user_id, update_time, update_user_id', 'safe', 'on'=>'search'),
@@ -78,6 +82,36 @@ class User extends CActiveRecord
 			'update_user_id' => 'Update User',
 		);
 	}
+
+
+    /**
+     * Checks if the given password is correct.
+     * @param string the password to be validated
+     * @return boolean whether the password is valid
+     */
+    public function validatePassword($password)
+    {
+        return $this->hashPassword($password)===$this->password;
+    }
+
+    /**
+     * apply a hash on the password before we store it in the database
+     */
+    protected function afterValidate()
+    {
+        parent::afterValidate();
+        if(!$this->hasErrors())
+            $this->password = $this->hashPassword($this->password);
+    }
+    /**
+     * Generates the password hash.
+     * @param string password
+     * @return string hash
+     */
+    public function hashPassword($password)
+    {
+        return md5($password);
+    }
 
 	/**
 	 * Retrieves a list of models based on the current search/filter conditions.
